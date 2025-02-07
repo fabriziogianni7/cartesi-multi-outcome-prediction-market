5f1ca855adc6dc4b628f9b2f553533ebe79de110
Shaheen <riseandshaheen@gmail.com>
2025-02-07 12:18:30 +0530

    refactor: added input payloadHash inside CounterCaller contract

--- contracts/test/CounterCaller.t.sol
+++ contracts/test/CounterCaller.t.sol
@@ -6,13 +6,13 @@ import {CounterCaller} from "../src/CounterCaller.sol";
 
 contract CounterCallerTest is Test {
     CounterCaller public counter;
-    address mockCoprocessor = address(0x1);
+    address mockTaskIssuer = address(0x1);
     bytes32 mockMachineHash = bytes32(uint256(1));
 
-    event ResultReceived(bytes output);
+    event ResultReceived(bytes32 indexed inputPayloadHash, bytes output);
 
     function setUp() public {
-        counter = new CounterCaller(mockCoprocessor, mockMachineHash);
+        counter = new CounterCaller(mockTaskIssuer, mockMachineHash);
     }
 
     function testInitialCount() external view {
@@ -30,13 +30,16 @@ contract CounterCallerTest is Test {
             mstore(add(notice, 32), 42)
         }
 
-        // Expect the ResultReceived event to be emitted
+        // Create a mock inputPayloadHash
+        bytes32 mockInputPayloadHash = bytes32(uint256(123));
+
+        // Expect the ResultReceived event to be emitted with both inputPayloadHash and notice
         vm.expectEmit(true, true, true, true);
-        emit ResultReceived(notice);
+        emit ResultReceived(mockInputPayloadHash, notice);
 
         // Since handleNotice is internal, we need to call it through a mock contract
-        MockCounterCaller mockCounter = new MockCounterCaller(mockCoprocessor, mockMachineHash);
-        mockCounter.exposedHandleNotice(notice);
+        MockCounterCaller mockCounter = new MockCounterCaller(mockTaskIssuer, mockMachineHash);
+        mockCounter.exposedHandleNotice(mockInputPayloadHash, notice);
 
         assertEq(mockCounter.count(), 42, "Count should be updated to 42");
     }
@@ -44,10 +47,10 @@ contract CounterCallerTest is Test {
 
 // Helper contract to test internal functions
 contract MockCounterCaller is CounterCaller {
-    constructor(address _coprocessorAddress, bytes32 _machineHash) 
-        CounterCaller(_coprocessorAddress, _machineHash) {}
+    constructor(address _taskIssuerAddress, bytes32 _machineHash) 
+        CounterCaller(_taskIssuerAddress, _machineHash) {}
 
-    function exposedHandleNotice(bytes memory notice) public {
-        handleNotice(notice);
+    function exposedHandleNotice(bytes32 inputPayloadHash, bytes memory notice) public {
+        handleNotice(inputPayloadHash, notice);
     }
 }
